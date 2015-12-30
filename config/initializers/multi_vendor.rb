10d6bc396b7ef79a48ee1317fbcf543047eb991c
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-30 14:32:47 +0100

    Added more permission sets to the vendor role.

--- config/initializers/multi_vendor.rb
+++ config/initializers/multi_vendor.rb
@@ -1,7 +1,9 @@
 Spree::RoleConfiguration.configure do |config|
   config.assign_permissions :vendor, [
     Spree::PermissionSets::OrderDisplay,
+    Spree::PermissionSets::OrderManagement,
     Spree::PermissionSets::UserDisplay,
+    Spree::PermissionSets::StockManagement,
     Spree::PermissionSets::ProductDisplay,
     Spree::PermissionSets::MultiVendor
   ]
