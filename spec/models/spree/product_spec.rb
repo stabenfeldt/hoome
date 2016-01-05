d27d055b08a426f8b1da613d319b042743a3d08e
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-05 12:09:25 +0100

    Test for items_belonging_to_user

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -130,6 +130,20 @@ describe Spree::Product, :type => :model do
       end
     end
 
+    context '#items_belonging_to_user' do
+        let(:product)        { create(:product) }
+        let(:user)           { create(:user) }
+        let(:variant)        { product.master }
+        let(:stock_location) { create(:stock_location) }
+        let(:stock_item)     { variant.stock_items.first }
+
+      it 'returns only products that belong to the same stock location as the user' do
+        user.stock_location_ids = [stock_location.id]
+        product.master.stock_location_ids = [stock_location.id]
+        expect( Spree::Product.items_belonging_to_user(user).first).to eq product
+      end
+    end
+
     context "#available?" do
       it "should be available if date is in the past" do
         product.available_on = 1.day.ago
