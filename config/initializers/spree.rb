49521fc7bf6a6d6b381b13f5749b4c6b56a1a823
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-21 14:05:34 +0100

    move config to the correct folder.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -49,6 +49,42 @@ Spree.config do |config|
   #   server: Rails.env.production? ? 'production' : 'test',
   #   test: !Rails.env.production?
   # )
+  #
+
+	attachment_config = {
+
+	  s3_credentials: {
+	    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+	    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+	    bucket:            ENV['S3_BUCKET_NAME']
+	  },
+
+	  storage:        :s3,
+	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	  s3_protocol:    "https",
+	  bucket:         ENV['S3_BUCKET_NAME'],
+	  url:            ":s3_domain_url",
+
+	  styles: {
+	      mini:     "48x48>",
+	      small:    "100x100>",
+	      product:  "240x240>",
+	      large:    "600x600>"
+	  },
+
+	  path:           "/:class/:id/:style/:basename.:extension",
+	  default_url:    "/:class/:id/:style/:basename.:extension",
+	  default_style:  "product"
+	}
+
+	attachment_config.each do |key, value|
+	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+	end
+
+
+# Note that I use the url: ":s3_domain_url" setting, this enabled the DNS lookup for your images without
+# specifying the specific zone endpoint. You need to use a bucket name that makes a valid subdomain.
+# So do not use dots if you are planning on using the DNS lookup config.
 end
 
 Spree.user_class = "Spree::LegacyUser"
