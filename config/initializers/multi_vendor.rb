68f50414ad765e1914285e567f614b0d6a7b9302
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-22 21:14:41 +0100

    dont load permission sets for vendor role.

--- config/initializers/multi_vendor.rb
+++ config/initializers/multi_vendor.rb
@@ -1,10 +1,10 @@
-Spree::RoleConfiguration.configure do |config|
-  config.assign_permissions :vendor, [
-    Spree::PermissionSets::OrderDisplay,
-    Spree::PermissionSets::OrderManagement,
-    Spree::PermissionSets::UserDisplay,
-    Spree::PermissionSets::StockManagement,
-    Spree::PermissionSets::ProductDisplay,
-    Spree::PermissionSets::MultiVendor
-  ]
-end
+#Spree::RoleConfiguration.configure do |config|
+#  config.assign_permissions :vendor, [
+#    Spree::PermissionSets::OrderDisplay,
+#    Spree::PermissionSets::OrderManagement,
+#    Spree::PermissionSets::UserDisplay,
+#    Spree::PermissionSets::StockManagement,
+#    Spree::PermissionSets::ProductDisplay,
+#    Spree::PermissionSets::MultiVendor
+#  ]
+#end
