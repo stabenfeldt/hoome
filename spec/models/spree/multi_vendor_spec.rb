cdf50d63f16d23ed22340502f899224d53322d15
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-23 17:39:42 +0100

    permission test

---
+++ spec/models/spree/multi_vendor_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Spree::PermissionSets::MultiVendor do
+  let(:ability) { Spree::Ability.new(user) }
+  let(:user)    { create(:user) }
+  let(:product) { create :product }
+	let(:stock_location) { create(:stock_location_with_items) }
+	let(:stock_item) { stock_location.stock_items.order(:id).first }
+
+  subject { ability }
+
+  context "when activated" do
+    before do
+      user.stock_locations = [stock_location]
+      described_class.new(ability).activate!
+    end
+
+    context "when the user is associated with the stock location" do
+      it { is_expected.to be_able_to(:manage,  Spree::StockItem) }
+      it { is_expected.to be_able_to(:manage,  Spree::Image) }
+      it { is_expected.to be_able_to(:manage,  Spree::Product) }
+    end
+
+    context "when the user is not associated with the stock location" do
+        before { user.stock_locations = [] }
+        it { is_expected.to_not be_able_to(:manage, Spree::StockItem) }
+    end
+
+  end
+
+end
