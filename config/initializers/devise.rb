aa747d5e804910bd5215a53ffb7663df05080f92
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-23 17:34:44 +0100

    dont’t load Devise in test.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,1 @@
-Devise.secret_key = "83338c17f78875810f4d4d3ae637f513bbaecdab49d320a8deacc65522804dc18d29b11e8e33fb206e0e1eecdf3a07c1aeac"
+Devise.secret_key = "83338c17f78875810f4d4d3ae637f513bbaecdab49d320a8deacc65522804dc18d29b11e8e33fb206e0e1eecdf3a07c1aeac" unless Rails.env.test?
