90c9bd7e85e6e30c515fd4fef84de184673b7376
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-25 13:51:20 +0100

    added Hoome logo :-D

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -28,6 +28,7 @@ Spree.config do |config|
 
 	# Custom logo for the frontend
 	# config.logo = "logo/solidus_logo.png"
+  config.logo = "logo/hoome.png"
 
 	# Template to use when rendering layout
 	# config.layout = "spree/layouts/spree_application"
@@ -36,7 +37,7 @@ Spree.config do |config|
 	# Admin:
 
 	# Custom logo for the admin
-	# config.admin_interface_logo = "logo/solidus_logo.png"
+  config.admin_interface_logo = "logo/hoome.png"
 
 	# Gateway credentials can be configured statically here and referenced from
 	# the admin. They can also be fully configured from the admin.
