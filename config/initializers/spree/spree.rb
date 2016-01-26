299bc9df7d99df5c524a7b3fceb61edcba9ad21f
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-26 14:47:23 +0100

    small admin logo

--- config/initializers/spree/spree.rb
+++ config/initializers/spree/spree.rb
@@ -37,7 +37,7 @@ Spree.config do |config|
 	# Admin:
 
 	# Custom logo for the admin
-  config.admin_interface_logo = "logo/hoome.png"
+  config.admin_interface_logo = "logo/hoome-small.png"
 
 	# Gateway credentials can be configured statically here and referenced from
 	# the admin. They can also be fully configured from the admin.
