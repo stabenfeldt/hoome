a290347a5a72ea8a67e53ea9ff8001c65968f64f
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-27 09:43:28 +0100

    Added admin? and matches_our_stock_location? helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def admin?
+    try_spree_current_user.admin?
+  end
+
+  def matches_our_stock_location?(stock_location_id)
+    try_spree_current_user.stock_location_ids.include? stock_location_id
+  end
+
 end
