504aa77201e0612a2f91d395309e5e21686b73f5
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-01 19:59:42 +0100

    order_displays_spec #WIP

--- spec/models/spree/permission_sets/order_display_spec.rb
+++ spec/models/spree/permission_sets/order_display_spec.rb
@@ -10,25 +10,49 @@ describe Spree::PermissionSets::OrderDisplay do
       described_class.new(ability).activate!
     end
 
-    it { is_expected.to be_able_to(:display, Spree::Order) }
-    it { is_expected.to be_able_to(:display, Spree::Payment) }
-    it { is_expected.to be_able_to(:display, Spree::Shipment) }
-    it { is_expected.to be_able_to(:display, Spree::Adjustment) }
-    it { is_expected.to be_able_to(:display, Spree::LineItem) }
-    it { is_expected.to be_able_to(:display, Spree::ReturnAuthorization) }
-    it { is_expected.to be_able_to(:display, Spree::CustomerReturn) }
-    it { is_expected.to be_able_to(:admin, Spree::Order) }
-    it { is_expected.to be_able_to(:admin, Spree::Payment) }
-    it { is_expected.to be_able_to(:admin, Spree::Shipment) }
-    it { is_expected.to be_able_to(:admin, Spree::Adjustment) }
-    it { is_expected.to be_able_to(:admin, Spree::LineItem) }
-    it { is_expected.to be_able_to(:admin, Spree::ReturnAuthorization) }
-    it { is_expected.to be_able_to(:admin, Spree::CustomerReturn) }
-    it { is_expected.to be_able_to(:edit, Spree::Order) }
-    it { is_expected.to be_able_to(:cart, Spree::Order) }
-    it { is_expected.to be_able_to(:display, Spree::Reimbursement) }
-    it { is_expected.to be_able_to(:display, Spree::ReturnItem) }
-    it { is_expected.to be_able_to(:display, Spree::Refund) }
+    context "when item belongs to current_user" do
+      it { is_expected.to be_able_to(:display, Spree::Order) }
+      it { is_expected.to be_able_to(:display, Spree::Payment) }
+      it { is_expected.to be_able_to(:display, Spree::Shipment) }
+      it { is_expected.to be_able_to(:display, Spree::Adjustment) }
+      it { is_expected.to be_able_to(:display, Spree::LineItem) }
+      it { is_expected.to be_able_to(:display, Spree::ReturnAuthorization) }
+      it { is_expected.to be_able_to(:display, Spree::CustomerReturn) }
+      it { is_expected.to be_able_to(:admin, Spree::Order) }
+      it { is_expected.to be_able_to(:admin, Spree::Payment) }
+      it { is_expected.to be_able_to(:admin, Spree::Shipment) }
+      it { is_expected.to be_able_to(:admin, Spree::Adjustment) }
+      it { is_expected.to be_able_to(:admin, Spree::LineItem) }
+      it { is_expected.to be_able_to(:admin, Spree::ReturnAuthorization) }
+      it { is_expected.to be_able_to(:admin, Spree::CustomerReturn) }
+      it { is_expected.to be_able_to(:edit, Spree::Order) }
+      it { is_expected.to be_able_to(:cart, Spree::Order) }
+      it { is_expected.to be_able_to(:display, Spree::Reimbursement) }
+      it { is_expected.to be_able_to(:display, Spree::ReturnItem) }
+      it { is_expected.to be_able_to(:display, Spree::Refund) }
+    end
+
+    context "when item does NOT belongs to current_user" do
+      it { is_expected.not_to be_able_to(:display, Spree::Order) }
+      #it { is_expected.not_to be_able_to(:display, Spree::Payment) }
+      #it { is_expected.not_to be_able_to(:display, Spree::Shipment) }
+      #it { is_expected.not_to be_able_to(:display, Spree::Adjustment) }
+      #it { is_expected.not_to be_able_to(:display, Spree::LineItem) }
+      #it { is_expected.not_to be_able_to(:display, Spree::ReturnAuthorization) }
+      #it { is_expected.not_to be_able_to(:display, Spree::CustomerReturn) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::Order) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::Payment) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::Shipment) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::Adjustment) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::LineItem) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::ReturnAuthorization) }
+      #it { is_expected.not_to be_able_to(:admin, Spree::CustomerReturn) }
+      #it { is_expected.not_to be_able_to(:edit, Spree::Order) }
+      #it { is_expected.not_to be_able_to(:cart, Spree::Order) }
+      #it { is_expected.not_to be_able_to(:display, Spree::Reimbursement) }
+      #it { is_expected.not_to be_able_to(:display, Spree::ReturnItem) }
+      #it { is_expected.not_to be_able_to(:display, Spree::Refund) }
+    end
   end
 
   context "when not activated" do
