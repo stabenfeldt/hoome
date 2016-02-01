0615a7fb8c085b146d38557454b64c30d20cae5e
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-01 19:52:42 +0100

    available_on default to Date.today

--- app/controllers/resource_controller_decorator.rb
+++ app/controllers/resource_controller_decorator.rb
@@ -1,6 +1,18 @@
 module Spree
   Admin::ResourceController.class_eval do
 
+    def new
+      invoke_callbacks(:new_action, :before)
+      @object.available_on = Date.today if @object.class == Spree::Product
+
+      respond_with(@object) do |format|
+        format.html { render :layout => !request.xhr? }
+        if request.xhr?
+          format.js   { render :layout => false }
+        end
+      end
+    end
+
     def collection
       return parent.send(controller_name) if parent_data.present?
       if model_class.respond_to?(:accessible_by) && !current_ability.has_block?(params[:action], model_class)
