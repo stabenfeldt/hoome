c4086530414b19cdc0068c0de1007d33ef588ed1
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-02 06:52:55 +0100

    add original sha to deface

--- app/overrides/deface_stock_table.rb
+++ app/overrides/deface_stock_table.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path  => "spree/admin/stock_items/_stock_management",
                      :replace       => "table.stock-table",
-                     :original      => '',
+                     :original      => '9c92ad783ceaec280b18ac7051ddd7f23631a1f3',
                      :partial       => "spree/admin/stock_items/stock_mangement",
                      :name          => "stock_table")
 
