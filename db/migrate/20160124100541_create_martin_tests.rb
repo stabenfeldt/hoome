21558a511559d05af74c69bf527b3c291825628a
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-04 14:52:10 +0100

    Added missing migrations by running.. rake railties:install:migrations rails g spree:install --sample=false
    https://github.com/solidusio/solidus/issues/789
    https://github.com/solidusio/solidus/issues/788

--- db/migrate/20160124100541_create_martin_tests.rb
+++
@@ -1,8 +0,0 @@
-class CreateMartinTests < ActiveRecord::Migration
-  def change
-    create_table :martin_tests do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124233550) do
+ActiveRecord::Schema.define(version: 20160204135303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,6 @@ ActiveRecord::Schema.define(version: 20160124233550) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
-  create_table "martin_tests", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "spree_addresses", force: :cascade do |t|
     t.string   "firstname"
