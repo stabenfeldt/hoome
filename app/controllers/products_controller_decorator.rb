4eed5495501701b86f1440bd833a0e6b963839d6
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-05 21:42:37 +0100

    added debug info

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -4,7 +4,10 @@ module Spree
     before_action :update_stock_location, only: :create
 
     def update_stock_location
+      Rails.logger.debug "\n update_stock_location \n"
       @product.master.stock_location_ids = [try_spree_current_user.stock_location_id]
+      Rails.logger.debug "Product is #{@product.inspect}"
+      Rails.logger.debug "Product is valid? #{@product.valid?}"
     end
 
   end
