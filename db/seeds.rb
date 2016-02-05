9aaa7201217a239c77d26fb4b9bb803bfd2a3b93
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-05 20:03:13 +0100

    Added Taxons to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or whered alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.where([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.where(name: 'Emanuel', city: cities.first)
 
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 
-# rake spree_auth:admin:create  # Create admin username and password
+# rake spree_auth:admin:where  # Create admin username and password
 
 unless Spree::Store.where(code: 'spree').exists?
   Spree::Store.new do |s|
-    s.code              = 'spree'
-    s.name              = 'Spree Demo Site'
-    s.url               = 'demo.spreecommerce.com'
-    s.mail_from_address = 'spree@example.com'
+    s.code              = 'Hoome'
+    s.name              = 'Hoome'
+    s.url               = 'beta.hoome.no'
+    s.mail_from_address = 'post@hoome.no'
   end.save!
 end
 
@@ -26,64 +26,64 @@ Spree::Role.where(name: "vendor").first_or_create
 Spree::Role.where(name: "user").first_or_create
 Spree::Role.where(name: "admin").first_or_create
 
-inne = Spree::Taxonomy.create(name: 'INNENDØRS')
-Spree::Taxon.create(name: 'Stue', 	 taxonomy_id: inne.id, parent: inne.root)
-Spree::Taxon.create(name: 'Kjøkken', taxonomy_id: inne.id, parent: inne.root)
-Spree::Taxon.create(name: 'Bad',     taxonomy_id: inne.id, parent: inne.root)
-Spree::Taxon.create(name: 'Soverom', taxonomy_id: inne.id, parent: inne.root)
+inne = Spree::Taxonomy.where(name: 'INNENDØRS').first_or_create
+Spree::Taxon.where(name: 'Stue', 	 taxonomy_id: inne.id, parent: inne.root).first_or_create
+Spree::Taxon.where(name: 'Kjøkken', taxonomy_id: inne.id, parent: inne.root).first_or_create
+Spree::Taxon.where(name: 'Bad',     taxonomy_id: inne.id, parent: inne.root).first_or_create
+Spree::Taxon.where(name: 'Soverom', taxonomy_id: inne.id, parent: inne.root).first_or_create
 
 
-stue = Spree::Taxonomy.create(name: 'STUE')
-Spree::Taxon.create(name: 'Sofaer', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Sofabord', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Sidebord', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Tepper', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Lenestoler', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Puff og gulvputer', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Puter', taxonomy_id: stue.id, parent: stue.root)
-Spree::Taxon.create(name: 'Belysning', taxonomy_id: stue.id, parent: stue.root)
+stue = Spree::Taxonomy.where(name: 'STUE').first_or_create
+Spree::Taxon.where(name: 'Sofaer', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Sofabord', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Sidebord', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Tepper', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Lenestoler', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Puff og gulvputer', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Puter', taxonomy_id: stue.id, parent: stue.root).first_or_create
+Spree::Taxon.where(name: 'Belysning', taxonomy_id: stue.id, parent: stue.root).first_or_create
 
-spiseplassen = Spree::Taxonomy.create(name: 'SPISEPLASSEN')
-Spree::Taxon.create(name: 'Spisebord', taxonomy_id: spiseplassen.id, parent: spiseplassen.root)
-Spree::Taxon.create(name: 'Spisestoler', taxonomy_id: spiseplassen.id, parent: spiseplassen.root)
-Spree::Taxon.create(name: 'Skjenker', taxonomy_id: spiseplassen.id, parent: spiseplassen.root)
-Spree::Taxon.create(name: 'Barmøbler', taxonomy_id: spiseplassen.id, parent: spiseplassen.root)
-Spree::Taxon.create(name: 'Belysning', taxonomy_id: spiseplassen.id, parent: spiseplassen.root)
+spiseplassen = Spree::Taxonomy.where(name: 'SPISEPLASSEN').first_or_create
+Spree::Taxon.where(name: 'Spisebord', taxonomy_id: spiseplassen.id, parent: spiseplassen.root).first_or_create
+Spree::Taxon.where(name: 'Spisestoler', taxonomy_id: spiseplassen.id, parent: spiseplassen.root).first_or_create
+Spree::Taxon.where(name: 'Skjenker', taxonomy_id: spiseplassen.id, parent: spiseplassen.root).first_or_create
+Spree::Taxon.where(name: 'Barmøbler', taxonomy_id: spiseplassen.id, parent: spiseplassen.root).first_or_create
+Spree::Taxon.where(name: 'Belysning', taxonomy_id: spiseplassen.id, parent: spiseplassen.root).first_or_create
 
-soverom = Spree::Taxonomy.create(name: 'SOVEROM')
-Spree::Taxon.create(taxonomy_id: soverom.id, parent: soverom.root, name: 'Madrasser og senger')
-Spree::Taxon.create(taxonomy_id: soverom.id, parent: soverom.root, name: 'Opbevaring')
-Spree::Taxon.create(taxonomy_id: soverom.id, parent: soverom.root, name: 'Tekstiler')
+soverom = Spree::Taxonomy.where(name: 'SOVEROM').first_or_create
+Spree::Taxon.where(taxonomy_id: soverom.id, parent: soverom.root, name: 'Madrasser og senger').first_or_create
+Spree::Taxon.where(taxonomy_id: soverom.id, parent: soverom.root, name: 'Opbevaring').first_or_create
+Spree::Taxon.where(taxonomy_id: soverom.id, parent: soverom.root, name: 'Tekstiler').first_or_create
 
-Spree::Taxonomy.create(name: 'BADEROM')
+Spree::Taxonomy.where(name: 'BADEROM').first_or_create
 
-barnerom = Spree::Taxonomy.create(name: 'BARNEROM')
-Spree::Taxon.create(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Seng')
-Spree::Taxon.create(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Barnemøbler')
-Spree::Taxon.create(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Barneartikler')
+barnerom = Spree::Taxonomy.where(name: 'BARNEROM').first_or_create
+Spree::Taxon.where(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Seng').first_or_create
+Spree::Taxon.where(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Barnemøbler').first_or_create
+Spree::Taxon.where(taxonomy_id: barnerom.id, parent: barnerom.root, name: 'Barneartikler').first_or_create
 
-oppbevaring = Spree::Taxonomy.create(name: 'OPPBEVARING')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Oppbevaringsmoduler')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Hyller')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Mediemøbler')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Kommoder')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Småoppbevaring')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Skjenker')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Reoler')
-Spree::Taxon.create(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Kurver')
+oppbevaring = Spree::Taxonomy.where(name: 'OPPBEVARING').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Oppbevaringsmoduler').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Hyller').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Mediemøbler').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Kommoder').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Småoppbevaring').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Skjenker').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Reoler').first_or_create
+Spree::Taxon.where(taxonomy_id: oppbevaring.id, parent: oppbevaring.root, name: 'Kurver').first_or_create
 
-interiør = Spree::Taxonomy.create(name: 'INTERIØR')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Belysning')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Tekstiler')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Tepper')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Dekor')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Til kjøkkenet')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Småmøbler')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Lykter/lysestaker')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Lys/servitter')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Bilder/rammer')
-Spree::Taxon.create(taxonomy_id: interiør.id, parent: interiør.root, name: 'Speil')
+interiør = Spree::Taxonomy.where(name: 'INTERIØR').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Belysning').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Tekstiler').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Tepper').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Dekor').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Til kjøkkenet').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Småmøbler').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Lykter/lysestaker').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Lys/servitter').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Bilder/rammer').first_or_create
+Spree::Taxon.where(taxonomy_id: interiør.id, parent: interiør.root, name: 'Speil').first_or_create
 
 
 
-Spree::TaxCategory.create(name: 'mva', description: 'merverdiavgift', is_default: true)
+Spree::TaxCategory.where(name: 'mva', description: 'merverdiavgift', is_default: true).first_or_create
