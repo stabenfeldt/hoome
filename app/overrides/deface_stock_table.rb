eeae92aa103c0779f7dfd18dc9faae7bfa0205cb
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-02-11 20:50:05 +0100

    update original.

--- app/overrides/deface_stock_table.rb
+++ app/overrides/deface_stock_table.rb
@@ -1,6 +1,5 @@
-Deface::Override.new(:virtual_path  => "spree/admin/stock_items/_stock_management",
-                     :replace       => "table.stock-table",
-                     :original      => '9c92ad783ceaec280b18ac7051ddd7f23631a1f3',
-                     :partial       => "spree/admin/stock_items/stock_mangement",
-                     :name          => "stock_table")
-
+Deface::Override.new(virtual_path:   'spree/admin/stock_items/_stock_management',
+                     replace:        'table.stock-table',
+                     original:       '44824ad2e0efcf676cfb8fdef3120d817536ff51',
+                     partial:        'spree/admin/stock_items/stock_mangement',
+                     name:           'stock_table')
