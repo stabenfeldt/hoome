9ca6b0d33645973e86be9c6042d4a083d66ab418
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-02 21:06:56 +0100

    All vendors beloning to an order gets an copy of the confirmation email.

---
+++ config/initializers/spree/order_mailer.rb
@@ -0,0 +1,15 @@
+Spree::OrderMailer.class_eval do
+  def confirm_email(order, resend = false)
+    @order = find_order(order)
+    @store = @order.store
+    subject = build_subject(Spree.t('order_mailer.confirm_email.subject'), resend)
+
+    # Fetch emails to the vendors:
+    vendors = @order.products.map do |p|
+      Spree::User.select { |u| u.stock_location_ids.first == p.master.stock_locations.first.id }
+    end
+    @vendor_emails = vendors.flatten.collect(&:email).uniq.join(',')
+
+    mail(to: @order.email, bcc: @vendor_emails,  from: from_address(@store), subject: subject)
+  end
+end
