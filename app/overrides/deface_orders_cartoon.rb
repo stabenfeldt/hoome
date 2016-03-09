09d2a393819a6245df076fdb84fbf570dbb65524
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-09 19:59:20 +0100

    rubocop

--- app/overrides/deface_orders_cartoon.rb
+++ app/overrides/deface_orders_cartoon.rb
@@ -1,6 +1,5 @@
-Deface::Override.new(:virtual_path  => "spree/admin/orders/_carton",
-                     :replace       => "[data-hook='admin_carton_form']",
-                     :original      => 'ab941c096f9e8e0eba7e25f4e1979e93a3c5f9c1',
-                     :partial       => "spree/admin/orders/carton",
-                     :name          => "admin_carton_form")
-
+Deface::Override.new(virtual_path:   'spree/admin/orders/_carton',
+                     replace:        "[data-hook='admin_carton_form']",
+                     original:       'ab941c096f9e8e0eba7e25f4e1979e93a3c5f9c1',
+                     partial:        'spree/admin/orders/carton',
+                     name:           'admin_carton_form')
