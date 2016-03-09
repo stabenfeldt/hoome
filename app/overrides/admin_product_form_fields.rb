26185db6ff65e14939f9da61a458f958f60f9162
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-09 19:31:06 +0100

    delete unused override

--- app/overrides/admin_product_form_fields.rb
+++
@@ -1,5 +0,0 @@
-#Deface::Override.new(virtual_path: 'spree/admin/products/_form',
-#                     replace:      "[data-hook='admin_product_form_fields']",
-#                     original:     '1e5ac694d17ee091111cdddf689cb130d1c87c5f',
-#                     partial:      'spree/admin/products/form',
-#                     name:         'admin_product_form_fields')
