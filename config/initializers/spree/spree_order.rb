7bb88bc14c71ad88a04bf46c4fcc64362e20a3ea
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-29 20:19:20 +0200

    display_total(user) calculates totalt for the users items.

--- config/initializers/spree/spree_order.rb
+++ config/initializers/spree/spree_order.rb
@@ -1,19 +1,17 @@
 Spree::Order.class_eval do
 
-
-  def self.belonging_to_user(user)
-    self.products.each do |p|
-      p.master.stock_locations_ids.include? user.stock_locations.first.id
-    end
-  end
-
-
   scope :belonging_to_user, -> (user) { includes(:products)
     .where(master: { stock_location_id: user.stock_locations.present? && user.stock_locations.first.id })
   }
 
-  # scope :items_belonging_to_user, -> (user) { includes(:stock_items)
-  #   .where( spree_stock_items: { stock_location_id: user.stock_locations.present? && user.stock_locations.first.id  } )
-  # }
+  # TODO Rename to products_belonging_to_user
+  def self.belonging_to_user(user)
+    self.products.each { |p| p.master.stock_locations_ids.include? user.stock_locations.first.id }
+  end
+
+  def display_total(user)
+    sum_users_products = self.line_items.select { |i| i.belongs_to_user?(user) }.sum(&:price)
+    Spree::Money.new(sum_users_products, { currency: self.currency })
+  end
 
 end
