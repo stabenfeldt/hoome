182139bc023ea558e8c99c967e9d00af477e3bed
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-30 19:34:39 +0200

    Ack is not available at Heroku. Use grep instead.

--- config/initializers/no-solidus-gem-in-development.rb
+++ config/initializers/no-solidus-gem-in-development.rb
@@ -1,6 +1,6 @@
 def correct_solidus
   # Match solidus pointing to a local version. (Not github or gem)
-  system("ack solidus Gemfile | ack -v '#' | ack path")
+  system("grep solidus Gemfile | grep -v '#' | grep path")
 end
 
 raise "Remember to point Gemfile to the local version of Solidus" unless correct_solidus or Rails.env.production?
