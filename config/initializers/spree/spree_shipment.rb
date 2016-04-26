af3c3e0d5320bdd919fa6bd9d122ec81bec902c6
Martin Stabenfeldt <you@example.com>
2016-04-26 23:30:08 +0200

    Testing ready_for_pickup #WIP

--- config/initializers/spree/spree_shipment.rb
+++ config/initializers/spree/spree_shipment.rb
@@ -1,19 +1,7 @@
-Spree::Order.class_eval do
+Spree::Shipment.class_eval do
 
-
-  def self.belonging_to_user(user)
-    self.products.each do |p|
-      p.master.stock_locations_ids.include? user.stock_locations.first.id
-    end
+  def ready_for_pickup!
+    self.update_attribute(:vendor_shipping_state, :ready_for_pickup)
   end
-
-
-  scope :belonging_to_user, -> (user) { includes(:products)
-    .where(master: { stock_location_id: user.stock_locations.present? && user.stock_locations.first.id })
-  }
-
-  # scope :items_belonging_to_user, -> (user) { includes(:stock_items)
-  #   .where( spree_stock_items: { stock_location_id: user.stock_locations.present? && user.stock_locations.first.id  } )
-  # }
 
 end
