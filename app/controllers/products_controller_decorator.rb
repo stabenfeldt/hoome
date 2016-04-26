518fd74305d7084fd54618594f9d9eca441745c0
Martin Stabenfeldt <you@example.com>
2016-04-26 23:31:42 +0200

    Price also needed.

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -7,6 +7,7 @@ module Spree
       Rails.logger.debug "\n update_stock_location \n"
       Rails.logger.debug "\n try_spree_current_user.stock_location_ids= #{try_spree_current_user.stock_location_ids} \n"
       @product.master.stock_location_ids = [try_spree_current_user.stock_location_id]
+      @product.price = params["product"]["price"]
       Rails.logger.debug "Product is #{@product.inspect}"
       Rails.logger.debug "Product is valid? #{@product.valid?}"
     end
