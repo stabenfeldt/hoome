c00f1a94c6bdff50dd9620cc93c660bf5bcdc478
Martin Stabenfeldt <you@example.com>
2016-04-27 16:54:21 +0200

    vendor_shipping_state helpers

--- config/initializers/spree/spree_shipment.rb
+++ config/initializers/spree/spree_shipment.rb
@@ -1,7 +1,31 @@
 Spree::Shipment.class_eval do
 
+  ####
+  #
+  # vendor_shipping_state can be one of these:
+  #
+  # - prepare_for_pickup
+  # - ready_for_pickup
+  # - sendt
+
+  def prepare_for_pickup?
+    self.vendor_shipping_state == 'prepare_for_pickup'
+  end
+
+  def ready_for_pickup?
+    self.vendor_shipping_state == 'ready_for_pickup'
+  end
+
   def ready_for_pickup!
-    self.update_attribute(:vendor_shipping_state, :ready_for_pickup)
+    self.update_attribute(:vendor_shipping_state, 'ready_for_pickup')
+  end
+
+  def sent!
+    self.update_attribute(:vendor_shipping_state, 'sent')
+  end
+
+  def sent?
+    self.vendor_shipping_state == 'sent'
   end
 
 end
