44b174a0529b43792abb24051cb36d58d04d8138
Martin Stabenfeldt <you@example.com>
2016-04-27 16:59:13 +0200

    vendor? and current_user helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
+
   def admin?
     try_spree_current_user.admin?
+  end
+
+  def vendor?
+    try_spree_current_user.vendor?
+  end
+
+  def current_user
+    try_spree_current_user
   end
 
   def matches_our_stock_location?(stock_location_id)
